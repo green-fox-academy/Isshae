873d88db577fe026ee59128cf2afe19133d47ef4
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-02-15 16:22:15 +0100

    task11 finished

--- Week1/day-04/src/task11.java
+++ Week1/day-04/src/task11.java
@@ -1,7 +1,7 @@
 public class task11 {
     public static void main(String[] args) {
         int a = 3;
-        a+=10;
+        a += 10;
         // make it bigger by 10
 
 
@@ -10,21 +10,21 @@ public class task11 {
 
         int b = 100;
         // make it smaller by 7
-        b-=7;
+        b -= 7;
 
         System.out.println(b);
 
 
         int c = 44;
         // please double c's value
-        c*=2;
+        c *= 2;
 
         System.out.println(c);
 
 
         int d = 125;
         // please divide by 5 d's value
-        d/=5;
+        d /= 5;
 
         System.out.println(d);
 
@@ -32,27 +32,26 @@ public class task11 {
         int e = 8;
         // please cube of e's value
 
-        e= e*e*e;
+        e = e * e * e;
         System.out.println(e);
 
 
         int f1 = 123;
         int f2 = 345;
         // tell if f1 is bigger than f2 (print as a boolean)
-        if (f1 >f2){
+        if (f1 > f2) {
             System.out.println(true);
-        }else{
+        } else {
             System.out.println(false);
         }
-
 
 
         int g1 = 350;
         int g2 = 200;
         // tell if the double of g2 is bigger than g1 (print as a boolean)
-        if ((g1*2)>(g2*2)) {
+        if ((g1 * 2) > (g2 * 2)) {
             System.out.println(true);
-        }else{
+        } else {
             System.out.println(false);
         }
 
@@ -60,10 +59,10 @@ public class task11 {
         int h = 135798745;
 
         // tell if it has 11 as a divisor (print as a boolean)
-        if (h%11==0){
+        if (h % 11 == 0) {
             System.out.println(true);
 
-        }else{
+        } else {
             System.out.println(false);
         }
 
@@ -71,25 +70,24 @@ public class task11 {
         int i1 = 10;
         int i2 = 3;
         // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
-        if ((i1>(i2*i2))&&(i1<(i2*i2*i2))){
+        if ((i1 > (i2 * i2)) && (i1 < (i2 * i2 * i2))) {
             System.out.println(true);
-        }else {
+        } else {
             System.out.println(false);
         }
 
         int j = 1521;
         // tell if j is dividable by 3 or 5 (print as a boolean)
-        if ((j%3==0)||(j%5==0)){
+        if ((j % 3 == 0) || (j % 5 == 0)) {
             System.out.println(true);
-        }else {
+        } else {
             System.out.println(false);
         }
 
 
         String k = "Apple";
         //fill the k variable with its cotnent 4 times
-        
 
-        System.out.println(k);
+        System.out.println(k+k+k+k);
     }
 }
