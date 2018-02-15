51b5b66909846ff3909ee36c0a7b7ef7e42016bc
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-02-15 12:23:07 +0100

    First commit of Java project

---
+++ Week1/day-04/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args){
+//01 - HelloMe
+
+
+        System.out.println("Hello Greg");
+//02 - HumptyDumpty
+        System.out.println("Humpty Dumpty sat on a wall,");
+        System.out.println("Humpty Dumpty had a great fall.");
+        System.out.println("All the king's horses and all the king's men");
+        System.out.println("Couldn't put Humpty together again.");
+//03 - HelloOthers
+
+     sout
+
+    }
+
+}
