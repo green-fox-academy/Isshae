3b09717514c7af2dc08c1d971555aecece10ab79
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-02-15 16:10:29 +0100

    task 11

---
+++ Week1/day-04/src/task7.java
@@ -0,0 +1,34 @@
+public class task7 {
+    public static void main(String[] args){
+        // Store your favorite number in a variable (as a number)
+        // And print it like this: "My favorite number is: 8"
+        int favoriteNumber=8;
+        System.out.println(favoriteNumber);
+
+        //task 8
+        // Swap the values of the variables
+        int a = 123;
+        int b = 526;
+        int c=a;
+
+        a=b;
+        b=c;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        //task 9
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String name = "Greg";
+        int age = 28;
+        double height = 1.79;
+        boolean married=true;
+
+
+    }
+}
