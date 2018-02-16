5e7c573ee33d45d46762275b8668d36e476c106c
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-02-16 11:23:40 +0100

    task25 completed

---
+++ Week1/day-04-05/src/task23.java
@@ -0,0 +1,21 @@
+public class task23 {
+    public static void main(String[] args) {
+        // Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+
+        for (int i = 0; i < 100; i++) {
+            System.out.print("I won't cheat on the exam!");
+        }
+        System.out.println();
+// Create a program that prints all the even numbers between 0 and 500
+
+        for (int i = 1; i < 500; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+
+}
