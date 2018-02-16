33bc961137acd52c5a5b56a66a3242ffba1f6cde
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-02-16 13:33:56 +0100

    task32 completed

---
+++ Week1/day-04-05/src/task32.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+
+public class task32 {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+// square like this:
+//
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %%%%%
+//
+// The square should have as many lines as the number was
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Feed me a number:");
+
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <=num ; j++) {
+                if (i==j){
+                    System.out.print("%");
+                }else if (i==1 || i==num || j==1 || j==num){
+                    System.out.print("%");
+                }else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+
+        /* for (int r = 1; r <= rows; r++) {
+            for (int c = 1; c <= rows; c++) {
+                if (c == r) {
+                    System.out.print("%");
+                } else if (r == 1 || r == rows || c == 1 || c == rows) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();*/
+
+        }
+    }
