dc42cb3012929f63b133eb043e9ed32cd6c08949
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-02-20 14:08:42 +0100

    <feat>(strings):reverse

---
+++ Week-02/day-02/src/com/company/reverse.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class reverse {
+    public static void main(String... args) {
+
+
+        // Create a method that can reverse a String, which is passed as the parameter
+        // Use it on this reversed string to check it!
+        // Try to solve this using charAt() first, and optionally anything else after.
+
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+        System.out.println(reverse(reversed));
+
+
+    }
+
+    public static String reverse(String reversed) {
+String reverse="";
+        for (int i = reversed.length() - 1; i >= 0; i--) {
+            reverse = reverse + reversed.charAt(i);
+
+        }
+        return reverse;
+    }
+
+
+}
+
+
+
+
