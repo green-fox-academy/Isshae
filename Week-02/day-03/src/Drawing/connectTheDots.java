a83bf7506d9d783437a6c3b417c75d03aaabe2a7
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-02-21 17:30:56 +0100

    <feat>(Drawing):connectTheDots started

---
+++ Week-02/day-03/src/Drawing/connectTheDots.java
@@ -0,0 +1,63 @@
+package Drawing;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Arrays;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class connectTheDots {
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that takes 1 parameter:
+        // An array of {x, y} points
+        // and connects them with green lines.
+        // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+        // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+        // {120, 100}, {85, 130}, {50, 100}}
+
+        int[][] box = {{10, 10}, {290, 10}, {290, 290}, {10, 290}};
+        int[][] lines = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}};
+
+        connectDots(box, graphics);
+
+    }
+
+    private static void connectDots(int[][] box, Graphics graphics) {
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < box.length; i++) {
+       
+            }
+
+        }
+
+
+
+    // Don't touch the code below
+    static int WIDTH = 300;
+    static int HEIGHT = 300;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
+
+
+// create a 300x300 canvas.
