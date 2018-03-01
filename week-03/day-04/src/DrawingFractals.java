26886b3a16f1fd67f8c5d1a3189b38c4aafb3062
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-03-01 16:05:17 +0100

    <feat>(Recursion):drawTree

---
+++ week-03/day-04/src/DrawingFractals.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class DrawingFractals {
+  private static void mainDraw(Graphics graphics) {
+    int x0 = WIDTH / 2;
+    int y0 = HEIGHT-50;
+    double length = 60;
+    double angle = 0;
+
+    drawTree(graphics, x0, y0, length, angle);
+  }
+
+  private static void drawTree(Graphics graphics, int x0, int y0, double length, double angle) {
+    if (length == 0) {
+      return;
+    } else {
+      int x1 = ((int) (x0 + length * Math.sin(Math.toRadians(angle))));
+      int y1 = ((int) (y0 - length * Math.cos(Math.toRadians(angle))));
+
+      graphics.setColor(colorRandomizer());
+      graphics.drawLine(x0, y0, x1, y1);
+
+      drawTree(graphics, x1, y1, length - 6, angle + 30);
+      drawTree(graphics, x1, y1, length - 6, angle - 30);
+    }
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 600;
+  static int HEIGHT = 600;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  private static Color colorRandomizer() {
+    Random random = new Random();
+    int colorRange = 256;
+    int r = random.nextInt(colorRange);
+    int g = random.nextInt(colorRange);
+    int b = random.nextInt(colorRange);
+    Color randomColor = new Color(r, g, b);
+
+
+    return randomColor;
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
