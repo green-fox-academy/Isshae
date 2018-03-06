98962cb124c1e0162875b0b92f92caf95d5c9ce3
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-03-06 12:23:49 +0100

    feat(Inheritance):Person Student Mentor class created

---
+++ week-04/day-02/src/greenFoxInheritance/Mentor.java
@@ -0,0 +1,39 @@
+package greenFoxInheritance;
+
+public class Mentor extends Person {
+  /*Create a Mentor class that has the same fields and methods as the Person class, and has the following additional
+
+fields:
+level: the level of the mentor (junior / intermediate / senior)
+methods:
+getGoal(): prints out "Educate brilliant junior software developers."
+introduce(): "Hi, I'm name, a age year old gender level mentor."
+The Mentor class has the following constructors:
+
+Mentor(name, age, gender, level)
+Mentor(): sets name to Jane Doe, age to 30, gender to female, level to intermediate*/
+
+  private String level;
+  private static String juniorLevel;
+  private static String intermediateLevel;
+  private static String seniorLevel;
+
+  public Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
+  }
+
+  public Mentor() {
+    level = intermediateLevel;
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + " , a " + age + " year old " + gender + level + " mentor.");
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+}
---
+++ week-04/day-02/src/greenFoxInheritance/Student.java
@@ -0,0 +1,63 @@
+package greenFoxInheritance;
+
+public class Student extends Person {
+  /*Create a Student class that has the same fields and methods as the Person class, and has the following additional
+
+fields:
+previousOrganization: the name of the student’s previous company / school
+skippedDays: the number of days skipped from the course
+methods:
+getGoal(): prints out "Be a junior software developer."
+introduce(): "Hi, I'm name, a age year old gender from previousOrganization who skipped skippedDays days from the course already."
+skipDays(numberOfDays): increases skippedDays by numberOfDays
+The Student class has the following constructors:
+
+Student(name, age, gender, previousOrganization): beside the given parameters, it sets skippedDays to 0
+Student(): sets name to Jane Doe, age to 30, gender to female, previousOrganization to The School of Life, skippedDays to 0*/
+
+  private String previousOrganization;
+  private int skippedDays;
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    skippedDays = 0;
+  }
+
+  public Student() {
+    super();
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + "year old " + gender + " from " + previousOrganization +
+            " who skipped " + skippedDays + " days from the course already.");
+  }
+
+  public void skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+  }
+
+  public String getPreviousOrganization() {
+    return previousOrganization;
+  }
+
+  public void setPreviousOrganization(String previousOrganization) {
+    this.previousOrganization = previousOrganization;
+  }
+
+  public int getSkippedDays() {
+    return skippedDays;
+  }
+
+  public void setSkippedDays(int skippedDays) {
+    this.skippedDays = skippedDays;
+  }
+}
