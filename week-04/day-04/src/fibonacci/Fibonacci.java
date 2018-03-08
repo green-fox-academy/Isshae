1bd7d26a2547aa0218d83d0e42562cbbd103550e
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-03-08 14:47:32 +0100

    feat(OO Test):Fibonacci skeleton

---
+++ week-04/day-04/src/fibonacci/Fibonacci.java
@@ -0,0 +1,8 @@
+package fibonacci;
+
+public class Fibonacci {
+  /*Write a function that computes a member of the fibonacci sequence by a given index
+Create tests that covers all types of input (like in the previous workshop exercise)*/
+
+
+}
