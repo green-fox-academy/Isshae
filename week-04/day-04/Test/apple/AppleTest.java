9ba091178d7a5b9fc353b197718798d336eee448
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-03-08 11:19:12 +0100

    feat(OO Test):AppleTest

---
+++ week-04/day-04/Test/apple/AppleTest.java
@@ -0,0 +1,20 @@
+package apple;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppleTest {
+
+  Apple apple;
+
+  @Before
+  public void setUp() {
+    apple = new Apple("apple");
+  }
+  @Test
+  public void getAppleTest() {
+assertEquals("apple",apple.getAppleString());
+  }
+}
