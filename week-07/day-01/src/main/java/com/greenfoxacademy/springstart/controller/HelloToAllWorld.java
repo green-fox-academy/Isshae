a4b79f3bbbf864480cbd33987f1ffef42c2e5ffc
Gergely Vajtai-Szellak <greg.vajtai@gmail.com>
2018-04-03 17:30:31 +0200

    feat(webApplication): say hello to all world started

---
+++ week-07/day-01/src/main/java/com/greenfoxacademy/springstart/controller/HelloToAllWorld.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.springstart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class HelloToAllWorld {
+
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+  @GetMapping("/web/greetingWorld")
+  public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model,
+                         Model model2) {
+    model.addAttribute("name", name);
+    model2.addAttribute("hello", hellos);
+    return "greetingToAllWorld";
+  }
+}
+
+
+
